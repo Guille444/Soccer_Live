0f37e8cc8cbd063d53f88a220ec48ebb816ed2b2
stanl <20220383@ricaldone.edu.sv>
2024-05-20 19:37:45 -0600

    Valoraciones admin

---
+++ api/models/handler/valoraciones_handler.php
@@ -0,0 +1,92 @@
+<?php
+// Se incluye la clase para trabajar con la base de datos.
+require_once('../../helpers/database.php');
+/*
+*	Clase para manejar el comportamiento de los datos de la tabla CLIENTE.
+*/
+class ValoracionHandler
+{
+    /*
+    *   Declaración de atributos para el manejo de datos.
+    */
+    protected $id = null;
+    protected $cliente = null;
+    protected $producto = null;
+    protected $descripcion = null;
+    protected $fecha = null; 
+    protected $estado = null;
+
+    public function changeStatus()
+    {
+        $sql = 'UPDATE comentarios
+                SET estado_valoracion = ?
+                WHERE id_comentario = ?';
+        $params = array($this->estado, $this->id);
+        return Database::executeRow($sql, $params);
+    }
+
+    /*
+    *   Métodos para realizar las operaciones SCRUD (search, create, read, update, and delete).
+    */
+    public function searchRows()
+    {
+        $value = '%' . Validator::getSearchValue() . '%';
+        $sql = 'SELECT id_comentario, descripcion_comentario, fecha_comentario
+                FROM comentarios
+                WHERE descripcion_comentario LIKE ? OR fecha_comentario LIKE ?
+                ORDER BY descripcion_comentario';
+        $params = array($value, $value, $value);
+        return Database::getRows($sql, $params);
+    }
+
+
+    public function readAll()
+    {
+        $sql = 'SELECT id_comentario, descripcion_comentario, fecha_comentario, estado_comentario
+                FROM comentarios
+                INNER JOIN clientes USING(id_cliente)
+                INNER JOIN productos USING(id_producto)
+                ORDER BY descripcion_comentario';
+        return Database::getRows($sql);
+    }
+
+    public function readOne()
+    {
+        $sql = 'SELECT id_comentario, descripcion_comentario, fecha_comentario, estado_comentario, id_cliente, id_producto
+                FROM comentarios
+                WHERE id_comentario = ?';
+        $params = array($this->id);
+        return Database::getRow($sql, $params);
+    }
+
+    public function updateRow()
+    {
+        $sql = 'UPDATE comentarios
+                SET estado_comentario = ?
+                WHERE id_comentario = ?';
+        $params = array($this->estado, $this->id);
+        return Database::executeRow($sql, $params);
+    }
+
+    public function readComentariosCliente()
+    {
+        $sql = 'SELECT id_comentario, descripcion_comentario, fecha_comentario
+                FROM comentarios
+                INNER JOIN clientes USING(id_cliente)
+                WHERE id_cliente = ? AND estado_comentario = true
+                ORDER BY fecha_comentario';
+        $params = array($this->cliente);
+        return Database::getRows($sql, $params);
+    }
+
+    public function readComentariosProducto()
+    {
+        $sql = 'SELECT id_comentario, descripcion_comentario, fecha_comentario
+                FROM comentarios
+                INNER JOIN productos USING(id_producto)
+                WHERE id_producto = ? AND estado_comentario = true
+                ORDER BY fecha_comentario';
+        $params = array($this->producto);
+        return Database::getRows($sql, $params);
+    }
+}
