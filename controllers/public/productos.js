c7ac4cf24a416f596c73aef32dffa10fadb5cca4
Antonio.M74 <20220418@ricaldone.edu.sv>
2024-05-29 11:55:54 -0600

    colortar

--- controllers/public/productos.js
+++ controllers/public/productos.js
@@ -24,14 +24,14 @@ document.addEventListener('DOMContentLoaded', async () => {
             // Se crean y concatenan las tarjetas con los datos de cada producto.
             PRODUCTOS.innerHTML += `
                 <div class="col-sm-12 col-md-6 col-lg-3 mt-4">
-                    <div class="card mb-3">
+                    <div class="card mb-3" id="cards">
                         <img src="${SERVER_URL}images/productos/${row.imagen_producto}" class="card-img-top" alt="${row.nombre_producto}">
                         <div class="card-body">
                             <h5 class="card-title">${row.nombre_producto}</h5>
                         </div>
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item">Precio unitario (US$): ${row.precio_producto}</li>
-                            <li class="list-group-item">Existencias: ${row.existencias_producto}</li>
+                        <ul class="list-group list-group-flush" id="lista">
+                            <li class="list-group-item"id="lista">Precio unitario (US$): ${row.precio_producto}</li>
+                            <li class="list-group-item"id="lista">Existencias: ${row.existencias_producto}</li>
                         </ul>
                         <div class="card-body text-center">
                             <a href="detalle.html?id=${row.id_producto}" class="btn btn-primary">Ver detalle</a>
